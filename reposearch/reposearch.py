7bd22c111d8637c208de38e700f3206860de7e24
Raul Caballero <raul.caballero@ucm.es>
2019-12-09 17:37:11 +0100

    Añadido busqueda por estrella y modificacion de constantes para configuracion

--- reposearch/reposearch.py
+++ reposearch/reposearch.py
@@ -7,21 +7,25 @@ TOKEN='token'
 USERNAME='user'
 BASE_URL='https://api.github.com/'
 SEARCH_URL='search/repositories'
-FORK_URL='repos/'
-def search_repos(keyword,updated='2019-11-01',total_items=1000):
-
-    SEARCH_URL='search/repositories'
-    QUERY_URL='?q='+keyword
-    PAGE='&page='
-    PER_PAGE='&per_page=100'
-    UPDATED='+pushed:>'+updated
-    TOTAL_ITEMS=total_items
-    PAGES = math.ceil(TOTAL_ITEMS/100)
+UPDATED = '2019-11-01'
+QUERY_URL = '?q='
+PER_PAGE=100
+NUM_ITEMS=100
+STARS=100
+NUM_PAGES_STR = '&page='
+def search_repos(keyword,updated=UPDATED,per_page=PER_PAGE,num_items=NUM_ITEMS, stars=STARS):
+    query_url=QUERY_URL+keyword
+    per_page_str='&per_page='+str(per_page)
+    updated_str='+pushed:>'+updated
+    stars_str='+stars:>'+str(stars)
+    num_pages=math.ceil(num_items/per_page)
     #### Programa
     count = 0
     result = []
-    for page in range(1,PAGES+1):
-        query=BASE_URL+SEARCH_URL+QUERY_URL+UPDATED+PER_PAGE+PAGE+str(page)
+    for page in range(1,num_pages+1):
+        num_pages_str=NUM_PAGES_STR+str(page)
+        query=BASE_URL+SEARCH_URL+query_url+updated_str+stars_str+per_page_str+num_pages_str
+        print(query)
         req = requests.get(query)
         repo_dict = req.json()['items']
         for i in repo_dict:
@@ -30,6 +34,7 @@ def search_repos(keyword,updated='2019-11-01',total_items=1000):
                     "name":i['name'],
                     "url":i['html_url'],
                     "owner": i['owner']['login'],
+                    "stars": i['stargazers_count'],
                 }
                 result.append(item.copy())
     header = result[0].keys()
@@ -37,16 +42,7 @@ def search_repos(keyword,updated='2019-11-01',total_items=1000):
     print("*** RESULTADOS ***")
     print(tabulate.tabulate(rows, header))
     return result
-
-def create_fork(owner,repo_name):
-    query=BASE_URL+FORK_URL+owner+'/'+repo_name+'/'+'forks'
-    req = requests.post(query,auth=(USERNAME, TOKEN))
-#def add_license(repo_name):
-#    git.Git
-#    pass
-def pull_request(repo):
-    pass
 def main():
-    search_repos('opensource')
+    search_repos('project')
     #create_fork('','-')
 main()
